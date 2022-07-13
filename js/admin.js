a189e04b1a95486e8c40988e2f6ff3abe2b0e8de
Francisco Perez <fperezdellarte@gmail.com>
2022-07-13 01:01:07 -0300

    cambios admin, index, navbar, 404 y about us

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,17 @@
+
 const doctors = JSON.parse(localStorage.getItem("doctors"))
+
+class Doctor{
+    constructor(id, name, image, mat, specialty, adress, phone ){
+    this.id = id;
+    this.name = name; 
+    this.image = image;
+    this.specialty = specialty;
+    this.mat = mat;
+    this.adress = adress;
+    this.phone = phone;
+    }
+}
 
 doctors.forEach(doctor => {
     const row = document.createElement("tr")
@@ -9,8 +22,56 @@ doctors.forEach(doctor => {
     <td>${doctor.mat}</td>
     <td>${doctor.adress}</td>
     <td>${doctor.phone}</td>
-    <td>-</td>
-    <td>❌</td>
-    <td>✍</td>
+    <td onclick="deleteDoctor(${doctor.id})">❌</td>
+    <td data-bs-toggle="modal" data-bs-target="#edit-modal" onclick="fillValues(${doctor.id})">✍</td>
+    
     `
+    document.querySelector("tbody").appendChild(row)
 });
+
+const addDoctor = (event) =>{
+    const nameDoctor = document.getElementById('nameDoctor').value;
+    const image = document.getElementById('image').value;
+    const especialidad = document.getElementById('especialidad').value;
+    const matricula = document.getElementById('matricula').value;
+    const direccion = document.getElementById('direccion').value;
+    const telefono = document.getElementById('telefono').value;
+    console.log(nameDoctor,especialidad,matricula,direccion,telefono);
+    let doctors = JSON.parse(localStorage.getItem('doctors'));
+    const newDoctor = new Doctor(doctors.length + 1,nameDoctor, image, matricula, especialidad, direccion, telefono);
+    doctors.push(newDoctor);
+    localStorage.setItem('doctors',JSON.stringify(doctors));
+}
+
+const deleteDoctor = (id) => {
+    let doctors = JSON.parse(localStorage.getItem('doctors'));
+    let doctorsUpdated = doctors.filter(doctors=>doctors.id !== id);
+    localStorage.setItem('doctors',JSON.stringify(doctorsUpdated));
+    window.location.reload();
+    }  
+
+    const fillValues = function(id){
+        let doctors = JSON.parse(localStorage.getItem('doctors'));
+        console.log(doctors);
+        let doctorToEdit = doctors.find(doctor => doctor.id == id);
+        console.log(doctorToEdit);
+        document.getElementById('nameDoctor-edit').value = doctorToEdit.name;
+        document.getElementById('especialidad-edit').value = doctorToEdit.especialidad;
+        document.getElementById('matricula-edit').value = doctorToEdit.matricula;
+        document.getElementById('direccion-edit').value = doctorToEdit.direccion;
+        document.getElementById('telefono-edit').value = doctorToEdit.telefono;
+        document.getElementById('edit-form').setAttribute('onsubmit',`editDoctor(${id})`);
+        }
+
+        const editDoctor = (id) =>{
+            let nameDoctor = document.getElementById('nameDoctor-edit').value;
+            let especialidad = document.getElementById('especialidad-edit').value;
+            let matricula = document.getElementById('matricula-edit').value;
+            let direccion = document.getElementById('direccion-edit').value;
+            let telefono = document.getElementById('telefono-edit').value;
+            let doctors = JSON.parse(localStorage.getItem('products'));
+            let doctorsUpdated = doctors.filter(doctor=>doctor.id !== id);
+            let doctorUpdated = new Doctor(id,nameDoctor, especialidad, matricula, direccion, telefono);
+            doctorsUpdated.push(doctorUpdated);
+            localStorage.setItem('doctors',JSON.stringify(doctorsUpdated));
+            }
--- js/medicos.js
+++ js/medicos.js
@@ -1,12 +1,10 @@
 class Doctor{
-    constructor(id, name, text, image, description, specialty, mat, adress, phone ){
+    constructor(id, name, image, mat, specialty, adress, phone ){
     this.id = id;
     this.name = name; 
-    this.text = text;
     this.image = image;
-    this.description = description;
-    this.specialty = specialty;
     this.mat = mat;
+    this.specialty = specialty;
     this.adress = adress;
     this.phone = phone;
     }
@@ -14,22 +12,22 @@ class Doctor{
 
 
 let doctors = [
-    new Doctor(1,'Dra. Andía González, Valeria','Traumatologia Cadera','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz_9TKZJWDGJOxVJyVQFLASzjjgPG4d4msBw&usqp=CAU','Especialista en Cirugía de Cadera - Cirugía Mini Invasivas - Microcirugías - Cirugía Percutanea - Reemplazos Articulares.','traumatologia'),
-    new Doctor(2, 'Dr. Ortega Enrique','Traumatologia Miembro Superior - Hombro','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsxYB_gMQISyl1I88w5F4Id8xPDzyqCWUu1A&usqp=CAU','','traumatologia'),
-    new Doctor(3,'Dra. Batista, Margarita','Traumatologia Miembro Inferior - Pie','https://depor.com/resizer/_oYeuarrAc5K2J3lhGis4VhBxow=/620x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/WFBWABMVJRCPNIONBNSZKFURL4.png','Especialista en cirugía de Tobillo y Pie - Miembro Titular-Acreditado de la SAMECIPP (Sociedad Argentina de Medicina y Cirugía de Pie y Pierna).','traumatologia'),
-    new Doctor(4, 'Dr. Pacios Jorge','Traumatologia y Ortopedia Infantil','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFa-i1308CqF4TnW7gfrkVqq5tUw72PQP6YA&usqp=CAU','','traumatologia'),
-    new Doctor(5,'Dr. Lescano, José Humberto','Cardiologo Clinico','https://www.tvmovie.de/bilder/758/2015/08/26/52739-patrick-dempsey-greys-anatomy.jpg?itok=YEpjnsaw','Especialista en Holter de Presión Arterial y Ecocardiografia','cardiologia'),
-    new Doctor(6,'Dr. Rodriguez, Roberto Fernando','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MlUNDlPckeg6kDYPYtcKCXHu6e96yPri4KDVbwaa-sYwWagONb_01i7YyAdhnZdMvIs&usqp=CAU','','cardiologia'),
-    new Doctor(7,'Dra. Cisneros, Mirta Susana','Cardiologa','https://gluc.mx/u/fotografias/m/2021/3/18/f1280x720-47525_179200_5050.jpg','Holter de presión arterial, Ecocardiografía, Electrocardiografía, Cardiología adulto','cardiologia'),
-    new Doctor(8,'Dra. Guerrero Molina, María Silvia','', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxeV09dpcQZ7PrmR-d0dq5vAvg_lt1Lf8nrCxHKiF18lDB6-zzSyTFihbUwr-iO7lzq2o&usqp=CAU','','cardiologia'),
-    new Doctor(9,'Dra. Moreno Lazarte, Marcela Cecilia','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYqBVZKG1lqS3eiiQCuR5RtEEc1SS-_uJgDxvuLoylh9HnV1g3X07QisCWOTLZ3iGlkFw&usqp=CAU','','clinica'),
-    new Doctor(10,'Dr. Morcos, Daniel Alberto','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYaOwBxl9zpesywpimuFBaqbil55_UzYlZGqYoI1WBswjdi7EbREsYIPIA5pDt9I6Jz3w&usqp=CAU','','clinica'),
-    new Doctor(11,'Dr. Tome Guzmán, Andrés Federico','','https://uvn-brightspot.s3.amazonaws.com/assets/vixes/btg/series.batanga.com/files/Los-7-hombres-mas-atractivos-de-Greys-Anatomy.jpg','','clinica'),
-    new Doctor(12,'Dra. Sabeh, Melisa Ianina','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjFEJsBwsQRbqLBtJuAfQo_tGxFHKlTr6vUQ&usqp=CAU','','clinica'),
-    new Doctor(13,'Dra. Ibáñez, Rossana del V.','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAZCCFmfQ2kitZ16X_6iC9CsyDkL0l1_l30wx2PEmUMzQJ4UtGuQ-zRslSVakEbeFyMQ&usqp=CAU','','neurologia'),
-    new Doctor(14,'Dr. Ordóñez, Hernán Esteban','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj4Tlscla6oL0mwJnj_w4ah5gS8B4Dg-UEMMFB8WLU9mWpKHG3SBfgL2FP7AmFxtnYCGg&usqp=CAU','','neurologia'),
-    new Doctor(15,'Dra. Batista, Margarita','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ve3YIL87KDue9j3rQy0jLV8I5aM13lr8LGxAqAK36i1VzleZAM6vkSczeBQ5apuvtiI&usqp=CAU','','neurologia'),
-    new Doctor(16,'Dr. Ovejero, Miguel Adolfo','','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz8oRnhCoT5IwMUvNVyWcswhrt3jk6e4HeNjDsQQPIaeG_0d3_W7NNz6r01HB9zpc7j8c&usqp=CAU','','neurologia')
+    new Doctor(1,'Dra. Andía González, Valeria','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz_9TKZJWDGJOxVJyVQFLASzjjgPG4d4msBw&usqp=CAU','12-344','traumatologia', "junin 543", "3814745385"),
+    new Doctor(2, 'Dr. Ortega Enrique','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsxYB_gMQISyl1I88w5F4Id8xPDzyqCWUu1A&usqp=CAU','11-344','traumatologia', "junin 743", "38147236885"),
+    new Doctor(3,'Dra. Batista, Margarita','https://depor.com/resizer/_oYeuarrAc5K2J3lhGis4VhBxow=/620x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/WFBWABMVJRCPNIONBNSZKFURL4.png','13-467','traumatologia', "Salta 543", "3814745485"),
+    new Doctor(4, 'Dr. Pacios Jorge','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFa-i1308CqF4TnW7gfrkVqq5tUw72PQP6YA&usqp=CAU','10-563','traumatologia', "junin 543", "3814745367"),
+    new Doctor(5,'Dr. Lescano, José Humberto','https://www.tvmovie.de/bilder/758/2015/08/26/52739-patrick-dempsey-greys-anatomy.jpg?itok=YEpjnsaw','10-353','cardiologia', "San Lorenzo 748", "3814565385"),
+    new Doctor(6,'Dr. Rodriguez, Roberto Fernando','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MlUNDlPckeg6kDYPYtcKCXHu6e96yPri4KDVbwaa-sYwWagONb_01i7YyAdhnZdMvIs&usqp=CAU','12-543','cardiologia', "jujuy 4233", "381474965"),
+    new Doctor(7,'Dra. Cisneros, Mirta Susana','https://gluc.mx/u/fotografias/m/2021/3/18/f1280x720-47525_179200_5050.jpg','13-5464','cardiologia', "Yrigoyen 583", "3814744685"),
+    new Doctor(8,'Dra. Guerrero Molina, María Silvia', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxeV09dpcQZ7PrmR-d0dq5vAvg_lt1Lf8nrCxHKiF18lDB6-zzSyTFihbUwr-iO7lzq2o&usqp=CAU','11-563','cardiologia', "Catamarca 765", "3814746795"),
+    new Doctor(9,'Dra. Moreno Lazarte, Marcela Cecilia','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYqBVZKG1lqS3eiiQCuR5RtEEc1SS-_uJgDxvuLoylh9HnV1g3X07QisCWOTLZ3iGlkFw&usqp=CAU','11-876','clinica',"Catamarca 125", "3814746795" ),
+    new Doctor(10,'Dr. Morcos, Daniel Alberto','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYaOwBxl9zpesywpimuFBaqbil55_UzYlZGqYoI1WBswjdi7EbREsYIPIA5pDt9I6Jz3w&usqp=CAU','14-657','clinica',"Corrientes 395", "3814745825" ),
+    new Doctor(11,'Dr. Tome Guzmán, Andrés Federico','https://uvn-brightspot.s3.amazonaws.com/assets/vixes/btg/series.batanga.com/files/Los-7-hombres-mas-atractivos-de-Greys-Anatomy.jpg','11-235','clinica',"Catamarca 1265", "3814564795" ),
+    new Doctor(12,'Dra. Sabeh, Melisa Ianina','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjFEJsBwsQRbqLBtJuAfQo_tGxFHKlTr6vUQ&usqp=CAU','14-543','clinica',"Las Piedras 245", "3814775795" ),
+    new Doctor(13,'Dra. Ibáñez, Rossana del V.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAZCCFmfQ2kitZ16X_6iC9CsyDkL0l1_l30wx2PEmUMzQJ4UtGuQ-zRslSVakEbeFyMQ&usqp=CAU','13-982','neurologia',"Rioja 955", "3814744695" ),
+    new Doctor(14,'Dr. Ordóñez, Hernán Esteban','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj4Tlscla6oL0mwJnj_w4ah5gS8B4Dg-UEMMFB8WLU9mWpKHG3SBfgL2FP7AmFxtnYCGg&usqp=CAU','11-766','neurologia',"Rioja 365", "3814746765" ),
+    new Doctor(15,'Dra. Batista, Margarita','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ve3YIL87KDue9j3rQy0jLV8I5aM13lr8LGxAqAK36i1VzleZAM6vkSczeBQ5apuvtiI&usqp=CAU','11-688','neurologia',"Av. Perón 2765", "3814778995" ),
+    new Doctor(16,'Dr. Ovejero, Miguel Adolfo','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz8oRnhCoT5IwMUvNVyWcswhrt3jk6e4HeNjDsQQPIaeG_0d3_W7NNz6r01HB9zpc7j8c&usqp=CAU','13-743','neurologia',"Av. Aconquija 1565", "3814749835" )
 ]
 
 localStorage.setItem('doctors',JSON.stringify(doctors));
@@ -49,7 +47,7 @@ doctors.forEach(doctor =>{
     </div>
     `
     let traumaContainer = document.getElementById('trauma-container');
-    traumaContainer.appendChild(doctorCard);
+    traumaContainer?.appendChild(doctorCard);
 }
 })
 doctors.forEach(doctor =>{
@@ -67,7 +65,7 @@ doctors.forEach(doctor =>{
     </div>
     `
     let cardioContainer = document.getElementById('cardio-container');
-    cardioContainer.appendChild(doctorCard);
+    cardioContainer?.appendChild(doctorCard);
 }
 })
 doctors.forEach(doctor =>{
@@ -85,7 +83,7 @@ doctors.forEach(doctor =>{
     </div>
     `
     let cardioContainer = document.getElementById('clinica-container');
-    cardioContainer.appendChild(doctorCard);
+    cardioContainer?.appendChild(doctorCard);
 }
 })
 doctors.forEach(doctor =>{
